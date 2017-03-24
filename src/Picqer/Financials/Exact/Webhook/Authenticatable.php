d295d687965798a28bddc0b80cfb46b5cbdc0331
Fogarasi Ferenc <ferenc.fogarasi@lynxsolutions.eu>
2017-03-24 15:43:02 +0200

    Extended Autheticatable trait's regexp

--- src/Picqer/Financials/Exact/Webhook/Authenticatable.php
+++ src/Picqer/Financials/Exact/Webhook/Authenticatable.php
@@ -5,7 +5,7 @@ trait Authenticatable
     public function authenticate($requestContent, $webhookSecret)
     {
         $matches = [];
-        $matched = preg_match('/^{"Content":(.*),"HashCode":"(.*)"}$/', $requestContent, $matches);
+        $matched = preg_match('/^{\s*"Content":\s*({.*}),\s*"HashCode":"(.*)"\s*}$/ms', $requestContent, $matches);
 
         if ($matched === 1 && isset($matches[1]) && isset($matches[2])) {
             return $matches[2] === strtoupper(hash_hmac('sha256', $matches[1], $webhookSecret));
